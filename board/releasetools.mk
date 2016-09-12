244ad4d331d75fb2c15d691befe57f2d04e2f436
HostZero <haneesh.da.rocker@gmail.com>
2016-09-12 18:18:51 +0530

    msm8996-common: fix paths for sepolicy and releasetools

--- board/releasetools.mk
+++ board/releasetools.mk
@@ -1,1 +1,1 @@
-TARGET_RELEASETOOLS_EXTENSIONS := device/cyanogen/msm8916-common
+TARGET_RELEASETOOLS_EXTENSIONS := device/zuk/msm8996-common
--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,1 +1,1 @@
-BOARD_SEPOLICY_DIRS += device/cyanogen/msm8916-common/sepolicy
+BOARD_SEPOLICY_DIRS += device/zuk/msm8996-common/sepolicy

0b3870c3338a4f363662c6c33139cbc84fb8bb3f
HostZero <haneesh.da.rocker@gmail.com>
2016-09-12 18:13:08 +0530

    msm8996-common: remove ramdisk offset and kernel tags

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,8 +2,6 @@
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x02000000
-BOARD_RAMDISK_OFFSET     := 0x02200000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
